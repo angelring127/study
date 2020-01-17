a2c531cb28ace6ec284c2eceb9bf3ec5749ea50c
Hoya <angelring127@gmail.com>
2020-01-17 18:32:08 +0900

    Add : App Counter

--- react-redux-hooks-tutorial/src/containers/CounterContainer.js
+++ react-redux-hooks-tutorial/src/containers/CounterContainer.js
@@ -1,3 +1,26 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import Counter from '../components/Counter';
 import { increment, decrement } from '../modules/counter';
+import { useSelector, useDispatch } from 'react-redux';
+
+const INCREMENT = 'counter/INCREMENT';
+const DECREMENT = 'counter/DECREMENT';
+
+const CounterContainer = () => {
+  const counter = useSelector(state => state.counter, []);
+  
+  const dispatch = useDispatch()
+  const onIncrement = useCallback(
+    () => dispatch({ type: INCREMENT }),
+    [dispatch]
+  )
+  const onDcrement = useCallback(
+    () => dispatch({ type: DECREMENT }),[dispatch]
+  );
+
+  return (
+    <Counter number={counter} onIncrease={onIncrement} onDecrease={onDcrement} />
+  );
+}
+
+export default CounterContainer;
